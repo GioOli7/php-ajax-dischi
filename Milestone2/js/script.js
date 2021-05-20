b40ad3c1493e80d9491791e8609b66a41898e035
GioOli7 <gio07live@gmail.com>
2021-05-20 15:58:01 +0200

    Milestone 2

---
+++ Milestone2/js/script.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+	el: '#app',
+	data: {
+		albums: [],
+	},
+	created() {
+		console.log(window.location.href);
+		const dataURL = window.location.href + 'database.php';
+		axios
+			.get(dataURL)
+			.then(result => {
+				this.albums = result.data;
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	},
+});
