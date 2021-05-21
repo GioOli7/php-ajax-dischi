46dfd3cb747b95c5b7aa9bbbb04cf89dd9cd5759
GioOli7 <gio07live@gmail.com>
2021-05-21 14:58:43 +0200

    bonus

--- Milestone2/data/getAlbum.php
+++ Milestone2/data/getAlbum.php
@@ -1,10 +1,35 @@
 <?php 
     include __DIR__ . './database.php';
 
+    $artists = [];
+    foreach ($database as $elem) {
+        if(!in_array($elem, $artists)){
+            $artists[] = $elem['author'];
+        }
+    }
 
+    $albumFiltered = [];
+    if(empty($_GET) || $_GET["author"] == "All" ){
+        $albumFiltered = $database;
+    }else{
+        foreach($database as $item){
+            // var_dump($item);
+            if($item["author"] === $_GET["author"]){
+                $albumFiltered[] = $item;
+            }
+        }
+    }
 
+    
+    $res = [
+        'artists' => $artists,
+        'database' => $albumFiltered,
+    ];
+    
+    
     // Content type
     header('Content-Type: application/json');
-
+    
     // preparazione dati Json
-    echo json_encode($database);
+    echo json_encode($res);
+    
--- Milestone2/js/script.js
+++ Milestone2/js/script.js
@@ -2,17 +2,28 @@ const app = new Vue({
     el: '#app',
     data: {
         albums: [],
+        select: 'All',
+        artists: [],
+        dataURL: window.location.href + 'data/getAlbum.php',
     },
     created() {
-        console.log(window.location.href);
-        const dataURL = window.location.href + 'data/getAlbum.php';
         axios
-            .get(dataURL)
+            .get(this.dataURL)
             .then(result => {
-                this.albums = result.data;
+                this.albums = result.data.database;
+                this.artists = result.data.artists;
             })
             .catch(err => {
                 console.log(err);
             });
+    },
+    methods: {
+        selected(select) {
+            //
+            axios.get(this.dataURL + `?author=${select}`)
+            .then(result => {
+                this.albums = result.data.database;
+            });
+        },
     },
 });
